e4c0e1245ede72a49db5e46c308e1e082d992f79
Rory Finnegan <rory@denvrdata.com>
2024-09-25 22:09:09 -0700

    Add a smoke test for the config

---
+++ tests/test_config.py
@@ -0,0 +1,53 @@
+# We just need to mock the Auth object
+import tempfile
+from unittest.mock import Mock, patch
+
+from denvr.config import config
+
+
+@patch("requests.post")
+def test_config(mock_post):
+    mock_post.return_value = Mock(
+        raise_for_status=lambda: None,
+        json=lambda: {
+            "result": {
+                "accessToken": "access1",
+                "refreshToken": "refresh",
+                "expireInSeconds": 60,
+                "refreshTokenExpireInSeconds": 3600,
+            },
+        },
+    )
+
+    # Realistic config file
+    content = """
+    [defaults]
+    server = "https://api.cloud.denvrdata.com"
+    api = "v2"
+    cluster = "Hou1"
+    tenant = "denvr"
+    vpcid = "denvr"
+    rpool = "reserved-denvr"
+    retries = 5
+
+    [credentials]
+    username = "test@foobar.com"
+    password = "test.foo.bar.baz"
+    """
+    with tempfile.NamedTemporaryFile(delete_on_close=False) as fp:
+        fp.write(content.encode())
+        fp.close()
+
+        print(fp.name)
+        print(type(fp))
+        conf = config(path=fp.name)
+
+        assert conf.auth._access_token == "access1"
+        assert conf.auth._refresh_token == "refresh"
+        assert conf.server == "https://api.cloud.denvrdata.com"
+        assert conf.api == "v2"
+        assert conf.cluster == "Hou1"
+        assert conf.tenant == "denvr"
+        assert conf.vpcid == "denvr"
+        assert conf.rpool == "reserved-denvr"
+        assert conf.retries == 5
