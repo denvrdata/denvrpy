51b79a9060993b3b4ee41ba86fbf179d903cfdd4
Rory Finnegan <rory@denvrdata.com>
2024-09-25 22:09:08 -0700

    Initial commit for MVP (no tests yet).

---
+++ denvr/api/v1/servers/virtual.py
@@ -0,0 +1,182 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from denvr.session import Session
+
+
+class Client:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_servers(
+        self,
+        cluster: str | None = None,
+    ):
+        """Get a list of virtual machines"""
+        kwargs = {
+            "params": {
+                "cluster": cluster,
+            },
+        }
+
+        return self.session.request(
+            "get",
+            "/api/v1/servers/virtual/GetServers",
+            **kwargs,
+        )
+
+    def get_server(
+        self,
+        id: str | None = None,
+        namespace: str | None = None,
+        cluster: str | None = None,
+    ):
+        """Get detailed information about a specific virtual machine"""
+        kwargs = {
+            "params": {
+                "id": id,
+                "namespace": namespace,
+                "cluster": cluster,
+            },
+        }
+
+        return self.session.request(
+            "get",
+            "/api/v1/servers/virtual/GetServer",
+            **kwargs,
+        )
+
+    def create_server(
+        self,
+        name: str | None = None,
+        rpool: str | None = None,
+        vpc: str | None = None,
+        configuration: str | None = None,
+        cluster: str | None = None,
+        ssh_keys: list | None = None,
+        operating_system_image: str | None = None,
+        personal_storage_mount_path: str | None = None,
+        tenant_shared_additional_storage: str | None = None,
+        persist_storage: bool | None = None,
+        direct_storage_mount_path: str | None = None,
+        root_disk_size: int | None = None,
+    ):
+        """Create a new virtual machine using pre-defined configuration"""
+        kwargs = {
+            "json": {
+                "name": name,
+                "rpool": rpool,
+                "vpc": vpc,
+                "configuration": configuration,
+                "cluster": cluster,
+                "ssh_keys": ssh_keys,
+                "operating_system_image": operating_system_image,
+                "personal_storage_mount_path": personal_storage_mount_path,
+                "tenant_shared_additional_storage": tenant_shared_additional_storage,
+                "persist_storage": persist_storage,
+                "direct_storage_mount_path": direct_storage_mount_path,
+                "root_disk_size": root_disk_size,
+            },
+        }
+
+        return self.session.request(
+            "post",
+            "/api/v1/servers/virtual/CreateServer",
+            **kwargs,
+        )
+
+    def start_server(
+        self,
+        id: str | None = None,
+        namespace: str | None = None,
+        cluster: str | None = None,
+    ):
+        """Start a virtual machine that has been previously set up and provisioned, but is currently OFFLINE"""
+        kwargs = {
+            "json": {
+                "id": id,
+                "namespace": namespace,
+                "cluster": cluster,
+            },
+        }
+
+        return self.session.request(
+            "post",
+            "/api/v1/servers/virtual/StartServer",
+            **kwargs,
+        )
+
+    def stop_server(
+        self,
+        id: str | None = None,
+        namespace: str | None = None,
+        cluster: str | None = None,
+    ):
+        """Stop a virtual machine, ensuring a secure and orderly shutdown of its operations within the cloud environment"""
+        kwargs = {
+            "json": {
+                "id": id,
+                "namespace": namespace,
+                "cluster": cluster,
+            },
+        }
+
+        return self.session.request(
+            "post",
+            "/api/v1/servers/virtual/StopServer",
+            **kwargs,
+        )
+
+    def destroy_server(
+        self,
+        id: str | None = None,
+        namespace: str | None = None,
+        cluster: str | None = None,
+    ):
+        """Permanently delete a specified virtual machine, effectively wiping all its data and freeing up resources for other uses"""
+        kwargs = {
+            "params": {
+                "id": id,
+                "namespace": namespace,
+                "cluster": cluster,
+            },
+        }
+
+        return self.session.request(
+            "delete",
+            "/api/v1/servers/virtual/DestroyServer",
+            **kwargs,
+        )
+
+    def get_configurations(
+        self,
+    ):
+        """Get detailed information on available configurations for virtual machines"""
+        kwargs = {}
+
+        return self.session.request(
+            "get",
+            "/api/v1/servers/virtual/GetConfigurations",
+            **kwargs,
+        )
+
+    def get_availability(
+        self,
+        cluster: str | None = None,
+        resource_pool: str | None = None,
+    ):
+        """Get information about the current availability of different virtual machine configurations"""
+        kwargs = {
+            "params": {
+                "cluster": cluster,
+                "resource_pool": resource_pool,
+            },
+        }
+
+        return self.session.request(
+            "get",
+            "/api/v1/servers/virtual/GetAvailability",
+            **kwargs,
+        )
---
+++ denvr/auth.py
@@ -0,0 +1,56 @@
+import os
+import time
+
+import requests
+
+
+class Auth(requests.auth.AuthBase):
+    """
+    Auth(server, username, password)
+
+    Handles authorization, renewal and logouts given a
+    username and password.
+    """
+
+    def __init__(self, server, username, password):
+        # Requests an initial authorization token
+        # storing the username, password, token / refresh tokens and when they expire
+        response = requests.post(
+            os.path.join(server, "api", "TokenAuth", "Authenticate"),
+            headers={"Content-type": "application/json"},
+            json={"userNameOrEmailAddress": username, "password": password},
+        )
+
+        # Just let requests raise an HTTPError if one occured
+        response.raise_for_status()
+        content = response.json()["result"]
+        self._server = server
+        self._access_token = content["accessToken"]
+        self._refresh_token = content["refreshToken"]
+        self._access_expires = time.time() + content["expireInSeconds"]
+        self._refresh_expires = time.time() + content["refreshTokenExpireInSeconds"]
+
+    @property
+    def token(self):
+        if time.time() > self._refresh_expires:
+            raise Exception("Auth refresh token has expired. Unable to refresh access token.")
+
+        if time.time() > self._access_expires:
+            response = requests.get(
+                os.path.join(self._server, "api", "TokenAuth", "RefreshToken"),
+                params={"refreshToken": self._refresh_token},
+            )
+            response.raise_for_status()
+            content = response.json()["result"]
+            self._access_token = content["accessToken"]
+            self._access_expires = time.time() + content["expireInSeconds"]
+
+        return self._access_token
+
+    def __call__(self, request):
+        request.headers["Authorization"] = "Bearer " + self.token
+        return request
+
+    def __del__(self):
+        # TODO: Add a logout request on auth object deletion
+        pass
