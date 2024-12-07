429eab31ad4b862df3340b9284659181cb8983c8
Rory Finnegan <rory@denvrdata.com>
2024-12-06 16:25:34 -0800

    Use pytest-httpserver for better unit test coverage.

--- tests/api/v1/test_clusters.py
+++ tests/api/v1/test_clusters.py
@@ -1,6 +1,8 @@
 from unittest.mock import Mock
 
 import pytest
+from pytest_httpserver import HTTPServer
+from pytest_httpserver.httpserver import UNDEFINED
 
 from denvr.api.v1.clusters import Client
 from denvr.config import Config
@@ -9,6 +11,9 @@ from denvr.validate import validate_kwargs
 
 
 def test_get_all():
+    """
+    Unit test default input/output behaviour when mocking the internal Session object.
+    """
     config = Config(defaults={}, auth=None)
 
     session = Mock()
@@ -35,8 +40,42 @@ def test_get_all():
     )
 
 
+def test_get_all_httpserver(httpserver: HTTPServer):
+    """
+    Test we're producing valid session HTTP requests
+    """
+    config = Config(
+        defaults={"server": httpserver.url_for("/")},
+        auth=None,
+    )
+
+    session = Session(config)
+    client = Client(session)
+
+    client_kwargs = {}
+
+    request_kwargs = validate_kwargs(
+        "get",
+        "/api/v1/clusters/GetAll",
+        {},
+        {},
+    )
+
+    # TODO: The request_kwargs response may break if we add schema validation on results.
+    httpserver.expect_request(
+        "/api/v1/clusters/GetAll",
+        method="get",
+        query_string=request_kwargs.get("params", None),
+        json=request_kwargs.get("json", UNDEFINED),
+    ).respond_with_json(request_kwargs)
+    assert client.get_all(**client_kwargs) == request_kwargs
+
+
 @pytest.mark.integration
-def test_integration_get_all(mock_config):
+def test_get_all_mockserver(mock_config):
+    """
+    Test our requests/responses match the open api spec with mockserver.
+    """
     session = Session(mock_config)
     client = Client(session)
 
