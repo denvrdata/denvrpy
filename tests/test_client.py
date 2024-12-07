9cbfe850a5a56dfde62c91fc2854853d9d8f01f2
Rory Finnegan <rory@denvrdata.com>
2024-12-06 17:44:31 -0800

    Add temp config file to the client test.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,6 @@
+import sys
+import tempfile
+
 from pytest_httpserver import HTTPServer
 
 from denvr.client import client
@@ -22,5 +25,24 @@ def test_client(httpserver: HTTPServer):
         },
     )
 
-    virtual = client("servers/virtual")
-    assert type(virtual).__name__ == "Client"
+    content = """
+    [defaults]
+    server = "{}"
+    api = "v2"
+    cluster = "Hou1"
+    tenant = "denvr"
+    vpcid = "denvr"
+    rpool = "reserved-denvr"
+    retries = 5
+
+    [credentials]
+    username = "alice@denvrtest.com"
+    password = "alice.is.the.best"
+    """.format(httpserver.url_for("/"))
+    kwargs = {"delete_on_close": False} if sys.version_info >= (3, 12) else {"delete": False}
+    with tempfile.NamedTemporaryFile(**kwargs) as fp:
+        fp.write(content.encode())
+        fp.close()
+
+        virtual = client("servers/virtual")
+        assert type(virtual).__name__ == "Client"
